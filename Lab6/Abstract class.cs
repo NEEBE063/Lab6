c35bccd6bdc61d1baca31b3d29d0bc763102cec9
vo992 <vo992@DESKTOP-2IIJ2BS>
2024-06-16 21:59:18 +0300

    Add project files.

---
+++ Lab6/Abstract class.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    public abstract class Planet
+    {
+        public abstract void ReportAboutMovement();
+        public abstract void ReportAboutLife();
+    }
+    public class Earth : Planet
+    {
+        public override void ReportAboutMovement()
+        {
+            Console.WriteLine("Земля рухається навколо Сонця");
+        }
+        public override void ReportAboutLife()
+        {
+            Console.WriteLine("На Землі є життя");
+        }
+    }
+    
+    public class Moon : Planet
+    {
+        public override void ReportAboutMovement()
+        {
+            Console.WriteLine("Місяць рухається навколо Землі");
+        }
+        public override void ReportAboutLife()
+        {
+            Console.WriteLine("На Місяці немає життя");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Planet planet;
+            planet = new Earth();
+            Console.WriteLine("Про Землю:");
+            planet.ReportAboutMovement();
+            planet.ReportAboutLife();
+            Console.WriteLine();
+            Console.WriteLine("Про Місяць:");
+            planet = new Moon();
+            planet.ReportAboutMovement();
+            planet.ReportAboutLife();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab6/Interface.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Program
+{
+    interface IPlanet
+    {
+        void ReportAboutMovement();
+        void ReportAboutLife();
+    }
+
+    class Earth : IPlanet
+    {
+        public void ReportAboutMovement()
+        {
+            Console.WriteLine("Земля рухається навколо Сонця");
+        }
+        public void ReportAboutLife()
+        {
+            Console.WriteLine("На Землі є життя");
+        }
+    }
+    class Moon : IPlanet
+    {
+        public void ReportAboutMovement()
+        {
+            Console.WriteLine("Місяць рухається навколо Землі");
+        }
+        public void ReportAboutLife()
+        {
+            Console.WriteLine("На Місяці немає життя");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        IPlanet planet = new Earth();
+        Console.WriteLine("Про Землю:");
+        planet.ReportAboutMovement();
+        planet.ReportAboutLife();
+        Console.WriteLine("Про Місяць:");
+        planet = new Moon();
+        planet.ReportAboutMovement();
+        planet.ReportAboutLife();
+    }
+}
